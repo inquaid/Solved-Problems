6f59ab1e29b676ce9f00f8a100aba4b5d1988be9
Azmain Inquaid Haque <azmaininquaidhaque@gmail.com>
2025-05-06 20:55:38 +0600

    init

---
+++ sublime/debug.h
@@ -0,0 +1,14 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#define bug(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << " = ", _do(__VA_ARGS__)
+template<typename T> void _do(vector<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void _do(set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void _do(unordered_set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename K, typename V> void _do(map<K, V> x){for(auto [key, value] : x) cerr << "{" << key << ": " << value << "} "; cerr << "\n";}
+template<typename T> void _do(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void _do(T && x, S&&...y) {cerr << x << ", "; _do(y...);}
+template<typename T> void _do(vector<pair<T,T>> x) {for(auto [first, second] : x) cerr << "\n{" << first << ", " << second << "}"; cerr << "\n"; }
+
+#endif // DEBUG_H
+
